8de0dfc1c39cd633f76edb185065c980733cb9e4
Jonathan Hasan <jonh.hasan@gmail.com>
2021-10-07 19:26:10 -0500

    Added diagnostic files to see rotary encoder count for different CountsPerRev, 178.8 gave the correct distance. Unsure of why default variable is failing. Maybe cause of the installation of the hall effect sensor. Project file updated with experimentation on parameters and ending with most recent parameter setup

--- Project_1a_DriveStraight90cm/Project_1a_DriveStraight90cm.ino
+++ Project_1a_DriveStraight90cm/Project_1a_DriveStraight90cm.ino
@@ -21,24 +21,74 @@
      ***********************************************************************/
 
      /**************
-      * Project 1: Optimal parameters for good run found to be targetcount = targetcount - 30, speed 160 (for now), offset 4 
+      * Project 1: 
+      * 1. Optimal parameters for good run found to be targetcount = targetcount - 38, speed 160 (for now), offset 4 
+      * 2. Optimal parameters for run 1 hold steady for speed of 180 
+      * 3. Parameters for 1 do not hold for speed of 190, increasing offset to 6 - worked well but trying 5 for better results. 
+      * Ended up performing worse than previously. Boosting to 6.5. Boosted to 7 and reduced targetcount to targetcount -= 32
+      * Then to 29 , offset to 6.5 
+      * 4. Decided to add in the second slow speed portion. Reducing speed of fast phase to 180, offset reduced to 4.5, slow speed is 120.
+      * TargetCount back to original status. Result: robot overshot by large margin. It might be the countsperrev is incorrect. 
+      * Changing to 188 instead of 192. 188 still overshot, changing to 184. Still overshot by a couple of centimeters. Reducing to 180.
+      * Still overshot a tiny fraction. Reducing to 179. Just barely missed it.increasing to 179.5. THis seemed to work perfectly
+      * Don't know why I needed to reduce the counter per rev. It should have been 192 but it overshoots beyond 90 cm. Maybe hall 
+      * effect sensor are not as well placed as originally though. 
+      * 
+      * Current  setup: CountsPerRev 179.5, slowing distance = 550, stop distance = 350, initial speed 180, slowing speed 120, target count
+      * untouched, offset = 4.5 
+      * 
+      * 5. Slowing distance to 650, stop to 250, slowing speed to 130. Bumping up counts per rev 180. 
+      * Time: 3.832 seconds  Distance from 90 cm: almost dead center (mm difference) 
+      * Time: 3.494      Distance from 90: multiple centimeter awa 
+      * TIme: 3.384 seconds   Distance: a centimeter
+      * TIme: 3.395 seconds   Distance: centimeter and a half 
+      * 
+      * Changing back to 179.5 
+      * 
+      * Time: 3.7699 seconds  Distance: a centimeter or so 
+      * 
+      * Moving around a bit much 
+      * 
+      * initial speed to 175, offset to 4 
+      * 
+      * A lot better, still off though reducing to 179.25
+      * 
+      * Going to 178
+      * 
+      * changed offset to 4.25 and then 4.65
+      * 
+      * Changed countsperrev to 177.5
+      * 
+      * Back to 178
+      * 
+      * Final setup: 
+      * 
+      * TIme: 3.7771   Distance: a centimeter
+      * 
+      * TIme: 3.919 seconds  Distance: couple centimeters 
+      * 
+      * 178.5 counts per rev and offset 4.5 
+      * 
+      * Offset to 4.3, countsperrev to 179.35
+      * 
+      * Final parameters - slowing distance: 650 , stop distance: 250, initial speed: 175, slowing speed: 130, offset 4.3, countsperrev: 178.8 
       */
     #include <RedBot.h>
     RedBotMotors motors;
 
     RedBotEncoder encoder = RedBotEncoder(A2, A3);
     int buttonPin = 12;
-    int countsPerRev = 192;   // 4 pairs of N-S x 48:1 gearbox = 192 ticks per wheel rev
+    int countsPerRev = 178.8;   // 4 pairs of N-S x 48:1 gearbox = 192 ticks per wheel rev. This should have worked but it overshot by half a foot 
 
     float wheelDiam = 65;  // diam = 65mm 
     float wheelCirc = PI*wheelDiam;  // Redbot wheel circumference = pi*D
     const float finalDistance = 900; // 90 cm, or 900 mm
-    const float slowingDistance = 900;
-   // const float stopDistance = 880;
-    const int initialSpeed = 160;
-    //const int slowingSpeed = 100;
+    const float slowingDistance = 650;
+    const float stopDistance = 250;
+    const int initialSpeed = 175;
+    const int slowingSpeed = 130;
       // variable used to offset motor power on right vs left to keep straight.
-    const int offset = 4;  // offset amount to compensate Right vs. Left drive
+    const int offset = 4.3;  // offset amount to compensate Right vs. Left drive
     const int motorDelay = 50; // small time delay to allow motors to respond to offset change
 
     void setup()
@@ -53,7 +103,7 @@
       if (digitalRead(buttonPin) == LOW)
       {
         driveStraight(slowingDistance, initialSpeed);
-        //driveStraight(stopDistance,slowingSpeed);
+        driveStraight(stopDistance,slowingSpeed);
         // now apply "brakes" to stop the motors.
         motors.brake();  
       }
@@ -76,7 +126,7 @@
       int rightPower = motorPower;
 
       numRev = distance / wheelCirc;  // calculate the target # of rotations
-      targetCount = numRev * countsPerRev - 38;    // calculate the target count
+      targetCount = numRev * countsPerRev;    // calculate the target count
 
       // debug
       Serial.print("driveStraight() ");
